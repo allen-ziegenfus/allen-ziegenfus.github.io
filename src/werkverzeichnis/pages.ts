3c0027e127f9a3f430525cbbba4f432e2d51577f
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-10-29 17:21:31 -0500

    Parse markdown

--- src/werkverzeichnis/pages.ts
+++ src/werkverzeichnis/pages.ts
@@ -2,45 +2,53 @@ import { listRecords, listPageRecords } from '../airtable/airtable.js';
 import * as fs from "fs";
 import * as dotenv from 'dotenv';
 import { slugify } from './slugify.js';
+import { marked } from 'marked';
+
 
 dotenv.config()
 
 async function getPages() {
-    const pages = [];
-    const pageRecords = await listPageRecords();
-    for (const pageRecord of pageRecords) {
-  
-      try {
-        const individualPageRecords = await listRecords(process.env.BASE_ID, pageRecord.fields.Table);
-        const reihenfolge = pageRecord.fields.Reihenfolge;
-        const page = {};
-        const processedPageRecords = []
-        let isTable = false;
-        for (const individualPageRecord of individualPageRecords) {
-          const processedPageRecord = {};
-          for (const field in individualPageRecord.fields) {
-              processedPageRecord[field] = individualPageRecord.fields[field]; 
-              if (field === 'Spalte1') { isTable = true}
+  const pages = [];
+  const pageRecords = await listPageRecords();
+  for (const pageRecord of pageRecords) {
+
+    try {
+      const individualPageRecords = await listRecords(process.env.BASE_ID, pageRecord.fields.Table);
+      const reihenfolge = pageRecord.fields.Reihenfolge;
+      const page = {};
+      const processedPageRecords = []
+      let isTable = false;
+      for (const individualPageRecord of individualPageRecords) {
+        const processedPageRecord = {};
+        for (const field in individualPageRecord.fields) {
+          if (field === 'Text') {
+            processedPageRecord[field] = marked.parse(individualPageRecord.fields[field]);
+          } else {
+            processedPageRecord[field] = individualPageRecord.fields[field];
           }
-          processedPageRecords.push(processedPageRecord);
-  
+          if (field === 'Spalte1') { isTable = true }
         }
-        processedPageRecords.sort((a, b) => a?.Reihenfolge ? a?.Reihenfolge - b?.Reihenfolge : 0);
-        page["Name"] = pageRecord.fields.Table;
-        page["Records"] = processedPageRecords;
-        page["Slug"] = `${slugify(pageRecord.fields.Table, {lower: true})}`;
-        page["isTable"] = isTable;
-        page["Reihenfolge"] = reihenfolge;
-        page["Kategorie"]= pageRecord.fields.Kategorie;
-        pages.push(page);
+        if (processedPageRecord["Reihenfolge"]) {
+        processedPageRecords.push(processedPageRecord);
+        }
+
       }
-      catch (error) {
-        console.log("error fetching records", error)
-      }
+      
+      page["Name"] = pageRecord.fields.Table;
+      page["Records"] = processedPageRecords.sort((a, b) => a?.Reihenfolge ? a?.Reihenfolge - b?.Reihenfolge : 0);;
+      page["Slug"] = `${slugify(pageRecord.fields.Table, { lower: true })}`;
+      page["isTable"] = isTable;
+      page["Reihenfolge"] = reihenfolge;
+      page["Kategorie"] = pageRecord.fields.Kategorie;
+      pages.push(page);
     }
-  
-    pages.sort((a, b) => a.Reihenfolge - b.Reihenfolge);
-    await fs.promises.writeFile("./public/pages.json", JSON.stringify(pages));
+    catch (error) {
+      console.log("error fetching records", error)
+    }
   }
 
-  await getPages();
+  pages.sort((a, b) => a.Reihenfolge - b.Reihenfolge);
+  await fs.promises.writeFile("./public/pages.json", JSON.stringify(pages));
+}
+
+await getPages();
