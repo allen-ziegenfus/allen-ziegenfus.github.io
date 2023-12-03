296da8186d40dbdda5d0153d3b88f23e4ff8634a
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-12-03 14:07:17 -0600

    Move search bar and add printer button to nav

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -68,7 +68,26 @@ export default function SearchBar({}) {
   });
 
   if (!open) {
-    return <button onClick={() => setOpen(true)}>🔍</button>;
+    return (
+      <button onClick={() => setOpen(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-search"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <circle cx="10" cy="10" r="7" />
+          <line x1="21" y1="21" x2="15" y2="15" />
+        </svg>
+      </button>
+    );
   } else {
     return (
       <div className="fixed top-0 bottom-0 left-0 right-0 bg-black overflow-scroll z-10">
